dcdd306f8cb6203a49a8f0b6c4a58e97328463e1
Eunice Min <euniceem1007@gmail.com>
2018-12-11 12:24:55 -1000

    done

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,15 @@
 module.exports = function acceptableSequence(str) {
   // write your code here
+  var letters = 'abcdefghijklmnopqrstuvwxyz';
+
+	for (var i = 0; i < str.length; i++) {
+    var isALetter = letters.includes(str[i]);
+		var noSymbolBeforeLetter = str[i - 1] !== '+';
+		var noSymbolAfterLetter = str[i + 1] !== '+';
+
+		if (isALetter && (noSymbolBeforeLetter || noSymbolAfterLetter)) {
+			return false;
+		}
+	}
+	return true;
 }
--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,20 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+var boo = true
+for(var i = 1; i<numArray.length; i++){
+  if(numArray[i] > numArray[i-1]){
+    boo = true
+  }else{
+    return false;
+  }
+  //if(numArray[i+1] > numArray[i])
+
 }
+  return boo;
+
+}
+
+//items in the array are in ascending order
+//data type array
+//check each item in the array
+//check for each unmber except for the first one
